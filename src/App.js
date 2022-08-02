0fb5fb33a5cf06b5402cb72c4101a1f1e8d10079
shaqayq <shaqayq.darwazi@gmail.com>
2022-08-02 17:44:30 +0430

    Change function base component  from App.js to class base component

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React from 'react';
 import './style.css';
 import Calculator from './components/Calculator';
 
-function App() {
-  return (
-    <Calculator />
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <Calculator />
+    );
+  }
 }
 
 export default App;
